49c49d2d7df1a5734c18eaedbb9a2fb6e24298ba
Simon Cousineau <simon.cousineau99@gmail.com>
2019-08-11 20:32:51 -0400

    Move code comments to README

--- README.md
+++ README.md
@@ -1,17 +1,46 @@
 # MarkdownToSlackFormatter
+
 Converts markdown to Slack's internal format 'mrkdwn' (See Slack API docs: https://api.slack.com/docs/message-formatting).
 
 # Usage
-``` ruby
+
+```ruby
 require 'SlackMrkdwn'
 
 slack_formatted_text = SlackMrkdwn.from('My markdown formatted text')
 ```
 
+_Note:_ Requires Ruby version above or equal to v2.4.6 (because it uses `match?`).
+
+# What is being converted?
+
+It will first encode Slack's restricted characters to HTML entities (`<`, `>` and `&`). Then it will convert:
+
+- Italics emphasis
+- Strong emphasis
+- Strike-through
+- Headings (to bold text)
+- Codeblocks
+- Indentation
+- Unordered lists
+- Links (Classic-style and reference-style)
+
+It will also strip any whitespace on either side once it's done the conversion.
+
+### What isn't converted?
+
+These elements do not need to be converted, either because Slack's style is the same or because Slack handles it by default.
+
+- Single line `codeblocks`
+- Blockquotes
+- Links on their own
+
 # Contributing
 
 ### Testing
+
 To run the test suite, execute:
-``` sh
+
+```sh
 rake test
 ```
--- lib/slack_mrkdwn.rb
+++ lib/slack_mrkdwn.rb
@@ -9,18 +9,10 @@ module SlackMrkdwn
       .gsub(/`{3}\w*(.*?)`{3}/m, '```\1```') # Codeblock
       .gsub(/^ +/, '\t') # Leading spaces
       .gsub(/^((?:\\t)*?)[\*\+-] (.+)/, '\1- \2') # Unordered lists
-      .gsub(/\[(.+?)\]\((.+?)(?: .*?)?\)/, '<\2|\1>') # Classic link TODO: Do not match repository references
+      .gsub(/\[(.+?)\]\((.+?)(?: .*?)?\)/, '<\2|\1>') # Classic link
       .gsub(/\[(.+?)\]\[(.+?)\]/) { |m| markdown.match?(/\[#{$2}\]: (.+)/i) ? ("|#{$1}>".prepend(/\[#{$2}\]: (.+)/i.match(markdown) { |n| "<#{$1}" })) : "[#{$1}][#{$2}]" } # Reference-style links
       .gsub(/\[(.+?)\]/) { |m| markdown.match?(/\[#{$1}\]: (.+)/i) ? ("|#{$1}>".prepend(/\[#{$1}\]: (.+)/i.match(markdown) { |n| "<#{$1}" })) : "[#{$1}]" } # Reference-style - Text only links
       .gsub(/\^<.+?|.+?>: .+/, '') # Remove link references
       .strip
-      # Ordered lists (TODO: Need to properly adjust numbers since they don't matter in markdown, also handle sublists)
-  
-      # Requires ruby >= 2.4.6 (because of `match?`)
-  
-      # Things that don't need to be modified:
-      # - single line `codeblocks`
-      # - Blockquotes
-      # - Links on their own (get turned into links by Slack)
-    end
+  end
 end
