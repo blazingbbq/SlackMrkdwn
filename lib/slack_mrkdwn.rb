10a52fd8047d97580af64186a0df6193651510b3
Simon Cousineau <simon.cousineau99@gmail.com>
2019-08-19 23:22:25 -0400

    Use Redcarpet renderer for conversion

--- lib/slack_mrkdwn.rb
+++ lib/slack_mrkdwn.rb
@@ -1,19 +1,130 @@
-module SlackMrkdwn
-  def self.from(markdown)
-    markdown
-      .gsub('&', '&amp;').gsub('<', '&lt;').gsub('>', '&gt;') # Encode Slack restricted characters
-      .gsub(/\*(.*?)\*/, '_\1_') # Emphasis
-      .gsub(/\*{2}(.+?)\*{2}/, '*\1*').gsub(/_{2}(.+?)_{2}/, '*\1*') # Strong emphasis
-      .gsub(/~{2}(.+?)~{2}/, '~\1~') # Strike-through
-      .gsub(/^\#{1,6} (.+?)$/, '*\1*').gsub(/(.*?)\n^=+$/, '*\1*').gsub(/(.*?)\n^-+$/, '*\1*') # Headings
-      .gsub(/`{3}\w*(.*?)`{3}/m, '```\1```') # Codeblock
-      .gsub(/^ +/, '\t') # Leading spaces
-      .gsub(/^((?:\\t)*?)[\*\+-] (.+)/, '\1- \2') # Unordered lists
-      .gsub(/\!\[.*?\]\((.+?)(?: .*?)?\)/, '\1') # Image
-      .gsub(/\[(.+?)\]\((.+?)(?: .*?)?\)/, '<\2|\1>') # Classic link
-      .gsub(/\[(.+?)\]\[(.+?)\]/) { |m| markdown.match?(/\[#{$2}\]: (.+)/i) ? ("|#{$1}>".prepend(/\[#{$2}\]: (.+)/i.match(markdown) { |n| "<#{$1}" })) : "[#{$1}][#{$2}]" } # Reference-style links
-      .gsub(/\[(.+?)\]/) { |m| markdown.match?(/\[#{$1}\]: (.+)/i) ? ("|#{$1}>".prepend(/\[#{$1}\]: (.+)/i.match(markdown) { |n| "<#{$1}" })) : "[#{$1}]" } # Reference-style - Text only links
-      .gsub(/\^<.+?|.+?>: .+/, '') # Remove link references
-      .strip
+# frozen_string_literal: true
+require 'redcarpet'
+
+class SlackMrkdwn < Redcarpet::Render::Base
+  # Methods where the first argument is the text content
+  [
+    # block-level calls
+    :block_html,
+
+    :autolink,
+    :raw_html,
+
+    :table, :table_row, :table_cell,
+
+    :superscript, :highlight,
+
+    # footnotes
+    :footnotes, :footnote_def, :footnote_ref,
+
+    :hrule,
+
+    # low level rendering
+    :entity, :normal_text,
+
+    :doc_header, :doc_footer,
+  ].each do |method|
+    define_method method do |*args|
+      args.first
+    end
+  end
+  
+  # Encode Slack restricted characters
+  def preprocess(content)
+    content.gsub('&', '&amp;').gsub('<', '&lt;').gsub('>', '&gt;')
+  end
+
+  def postprocess(content)
+    content.rstrip
+  end
+
+  # ~~strikethrough~~
+  def strikethrough(content)
+    "~#{content}~"
+  end
+
+  # _italic_
+  def underline(content)
+    "_#{content}_"
+  end
+
+  # *italic*
+  def emphasis(content)
+    "_#{content}_"
+  end
+
+  # **bold**
+  def double_emphasis(content)
+    "*#{content}*"
+  end
+
+  # ***bold and italic***
+  def triple_emphasis(content)
+    "*_#{content}_*"
+  end
+
+  # ``` code block ```
+  def block_code(content, _language)
+    "```\n#{content}```\n"
+  end
+
+  # > quote
+  def block_quote(content)
+    "&gt; #{content}"
+  end
+
+  # `code`
+  def codespan(content)
+    "`#{content}`"
+  end
+
+  # links
+  def link(link, _title, content)
+    "<#{link}|#{content}>"
+  end
+
+  # lists
+  def list(entries, list_type)
+    reset_list(list_type)
+    entries
+  end
+
+  def reset_list(list_type)
+    @list_item_count = 0 if list_type == :ordered
+  end
+
+  def list_prefix(list_type)
+    return "-" if list_type == :unordered
+    @list_item_count = 0 unless @list_item_count
+    @list_item_count += 1
+    "#{@list_item_count}."
+  end
+
+  def list_item(entry, list_type)
+    "#{list_prefix(list_type)} #{entry}"
+  end
+
+  # ![](image)
+  def image(link, _title, _alt_text)
+    link
+  end
+
+  def linebreak()
+    "\n"
+  end
+
+  def paragraph(text)
+    "#{text}\n"
+  end
+
+  def header(text, _header_level)
+    "*#{text}*\n"
+  end
+
+  class << self
+    def from(markdown)
+      renderer = SlackMrkdwn.new
+      Redcarpet::Markdown.new(renderer, strikethrough: true, underline: true, fenced_code_blocks: true).render(markdown)
+    end
   end
 end
--- spec/slack_mrkdwn_spec.rb
+++ spec/slack_mrkdwn_spec.rb
@@ -8,17 +8,20 @@ describe SlackMrkdwn do
     Test.new('encodes Slack\'s restricted characters', '<Restricted&Characters>', '&lt;Restricted&amp;Characters&gt;'),
     Test.new('converts emphasis', '*Asterisk* and _underscore_ emphasis', '_Asterisk_ and _underscore_ emphasis'),
     Test.new('converts strong emphasis', '**Asterisk** and __underscore__ strong emphasis', '*Asterisk* and *underscore* strong emphasis'),
-    Test.new('converts combined emphasis', 'Combined emphasis with **asterisks and _underscores_**.', 'Combined emphasis with *asterisks and _underscores*_.'),
+    Test.new('converts combined emphasis', 'Combined emphasis with **asterisks and _underscores_**.', 'Combined emphasis with *asterisks and _underscores_*.'),
     Test.new('converts strike-through', '~~Crossed out~~ content', '~Crossed out~ content'),
     Test.new('converts codeblocks', "```javascript\nconsole.log('Hello, World!')\n```", "```\nconsole.log('Hello, World!')\n```"),
-    Test.new('converts leading spaces', '   Aligned text content', '\tAligned text content'),
-    Test.new('converts unordered lists', "+ List with plus symbol\n* Or asterisk\n- Or minus symbol\n\\t+ Even indented lists", "- List with plus symbol\n- Or asterisk\n- Or minus symbol\n\\t- Even indented lists"),
+    Test.new('converts unordered lists', "+ List with plus symbol\n* Or asterisk\n- Or minus symbol\n\t+ Even indented lists", "- List with plus symbol\n- Or asterisk\n- Or minus symbol\n- Even indented lists"),
+    Test.new('converts ordered lists', "1. First element\n1. Second element\n1. Third element", "1. First element\n2. Second element\n3. Third element"),
     Test.new('converts image', "![](https://example.com/image.png)", 'https://example.com/image.png'),
     Test.new('converts image with text', "![ignored](https://example.com/image.png)", 'https://example.com/image.png'),
     Test.new('converts classic-style links', '[Classic-style link](https://www.google.com)', '<https://www.google.com|Classic-style link>'),
     Test.new('converts classic-style links with alt text', '[Classic-style link](https://www.google.com "With alt text")', '<https://www.google.com|Classic-style link>'),
     Test.new('converts reference-style links', "[Reference-style link][2]\n[2]: https://www.google.com", '<https://www.google.com|Reference-style link>'),
     Test.new('converts reference-style, text only links', "[Reference-style link, text only]\n[reference-style link, text only]: https://www.google.com", '<https://www.google.com|Reference-style link, text only>'),
+
+    Test.new('does not convert leading spaces', '   Aligned text content', "   Aligned text content"),
+    Test.new('does not convert paragraphs', 'My paragraph looks the same!', 'My paragraph looks the same!'),
   ]
 
   tests.each do |t|
